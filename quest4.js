ae7c5235e538ef392f3cff176568dfcf372db3ae
Mohit Mendiratta <mohitmendi1990@gmail.com>
2019-05-23 09:51:21 +0530

    code updated for quest4

--- quest4.js
+++ quest4.js
@@ -1,13 +1,39 @@
-var priceOfProduct = [39,12,33,10];
-var i;
+var priceOfProduct = [70,69,87,1];
+var i,j;
+
+// for(i=0;i<priceOfProduct.length;i++){
+
+//     if(priceOfProduct[i]>priceOfProduct[i+1]){
+
+// for(j=1;j<priceOfProduct.length-1;j++){
+
+//     if(priceOfProduct[j]>priceOfProduct[j+1]){
+
+// console.log("Sorting is High to Low");
+//     }
+    
+//     else{
+//         console.log("Sorting is not High to Low2");
+//         break;
+//     }
+// }
+//  }
+
+//     else{
+//        console.log("Sorting is not High to Low1");
+//         break;
+//     }
+// }
 
 for(i=0;i<priceOfProduct.length;i++){
-    if(priceOfProduct[i]>priceOfProduct[i+1]&& priceOfProduct[i]>priceOfProduct[i+2]){
-console.log("Sorting is High to Low");
- }
-    else{
-        console.log("Sorting is not High to Low");
+    for(j=i+1;j<priceOfProduct.length;j++){
+        if(priceOfProduct[i]>priceOfProduct[j]){
+            console.log("Sorting is high to low");
+        }
+            else{
+                console.log("Sorting is not high to low");
+                break;
+            }
+        }
+        break;
     }
-}
-
-
